3a69430201e1fcd38e3d166ef00d99e8888e3194
Shadats <sebastian_boari@hotmail.com>
2019-06-10 18:33:30 -0300

    rehaciendo projecto y viendo components

---
+++ resources/js/components/ClienteComponent.vue
@@ -0,0 +1,42 @@
+<template>
+		<div class="container">
+			<button @click="altacliente=true">Ingresar</button>
+			  <table class="table" style="font">
+				<th>
+					<td>ID</td>
+					<td>Nombre</td>
+				</th>
+				<tr v-for="cliente in clientes">
+					<td>{{cliente.id}}</td>
+					<td>{{cliente.name}}</td>
+				</tr>
+			  </table>
+			<cliente-component v-if="altacliente"></cliente-component>
+		</div>
+</template>
+<script>
+	export default {
+
+		data(){
+			return{
+				altacliente:false,
+				clientes:[]
+			}
+		},
+
+		mounted(){
+			this.mostrarCliente();
+			console.log('Component mounted.');
+		},
+
+		methods:{
+			mostrarCliente:function(){
+				axios.get('cliente').then(respose =>{
+					this.clientes = respose.data
+				})
+			}
+		}	
+
+	}
+</script>
+
